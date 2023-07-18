e63f0b9e98eb1ea9cc0ec638501343931eb87d22
Francisco Leiva <francisco.leiva.ua@gmail.com>
2023-07-18 16:29:48 -0400

    Avance controlador de Acceso
    Se agregaron métodos de búsqueda y comparación de datos. Se modifico el html de index para que se desplegué el mensaje.

--- PGM ORM/Controllers/AcessoController.cs
+++ PGM ORM/Controllers/AcessoController.cs
@@ -6,22 +6,57 @@ namespace PGM_ORM.Controllers
 {
     public class AcessoController : Controller
     {
+        private readonly OrmcrudContext _context;
+
+        public AcessoController(OrmcrudContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Validate(string Clave, string Correo)
+        public ActionResult Validate(string? Clave, string? Correo)
         {
             var correo = Correo;
             var clave = Clave;
 
-            if (correo == "" || clave == "")
+            //Condicional para ver si clave o correo están vacías o nulas. 
+            if (string.IsNullOrEmpty(correo) || string.IsNullOrEmpty(clave))
             {
-               
+                TempData["Error"] = "Usuario o contraseña Invalida, intenta de nuevo.";
+                return RedirectToAction("Index","Acesso");
             }
+
+            //Búsqueda de usuario utilizando el correo ingresado desde el formulario. 
+            var v_usuario = _context.Usuarios.FirstOrDefault(m => m.Correo == correo);
+
+            if (v_usuario == null)
+            {
+                //Como no se encontró un usuario con ese correo se envia mensaje de error.
+                TempData["Error"] = "Correo electrónico no existe.";
+                return RedirectToAction("Index", "Acesso");
+            }
+
+            //Se obtiene la clave del usuario encontrado
+            var v_clave = v_usuario.Clave;
+
+            //Comprobar contraseñas de formulario y modelo.
+            if(clave != v_clave)
+            {
+                //Como las contraseñas no coinciden se envia al login nuevamente con mensaje de error.
+                TempData["Error"] = "La contraseña no es valida.";
+                return RedirectToAction("Index", "Acesso");
+                
+            }
+
+            //Falta agregar datos de la sesión
             return RedirectToAction("Index", "Home");
+
+
         }
     }
 }
